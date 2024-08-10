87a712be8cfed219a8f2d1326a3fd567c6d7b9eb
infraket <55091681+infraket@users.noreply.github.com>
2024-08-10 15:17:11 +0300

    Create task_1.py

---
+++ lg/task_1.py
@@ -0,0 +1,13 @@
+def isEven(value):
+    return value % 2 == 0
+
+
+def isEven2(num):
+    return num % 10 in (0, 2, 4, 6, 8)
+
+
+def isEven3(num):
+    return (num & 1) == 0
+
+
+print(isEven2(7))
